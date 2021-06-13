c2534b28cc6b5d257a33eaa8cc892fa3c5845d0b
JanaShen <31425226+JannaShen@users.noreply.github.com>
2021-06-13 13:33:01 +1000

    update

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,14 +7,14 @@ export default function Dictionary(){
     let [result, setResult]=useState(null);
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+       
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
 
     }
 
     function handleResponse(response){
-        console.log(response.data[0]);
+        
         setResult(response.data[0])
 
     }
