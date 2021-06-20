1eb451fe17de8aa4b82c309b8cae1c3c0da32cc2
JanaShen <31425226+JannaShen@users.noreply.github.com>
2021-06-20 11:43:54 +1000

    update style

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,10 +3,11 @@ import React from "react";
 export default function Phonetic(props){
     return(
         <div className="Phonetic">
-            <a href={props.phonetic.audio}>Listen</a>
-            <br />
+            <a href={props.phonetic.audio}><i class="fas fa-volume-up"></i></a>
             {props.phonetic.text}
         </div>
+
+       
     )
 
 }
